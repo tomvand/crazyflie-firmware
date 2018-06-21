13eb69b3a184e17564bd619f050011bff01d2157
Tom van Dijk <tomvand@users.noreply.github.com>
2018-06-21 15:08:26 +0200

    WIP UNTESTED send setpoints from homing vector

--- src/deck/drivers/src/stereoboard.c
+++ src/deck/drivers/src/stereoboard.c
@@ -25,7 +25,6 @@
  */
 #define DEBUG_MODULE "StereoboardDeck"
 
-
 #include "stereoboard.h"
 #include "zranger.h"
 #include "arm_math.h"
@@ -33,6 +32,9 @@
 #include "estimator_kalman.h"
 #include "param.h"
 
+#include "commander.h"
+
+#define COMMANDER_PRIORITY_HOMING 3
 
 uint16_t range_last;
 
@@ -41,98 +43,73 @@ static float velx, vely, velz;
 static float homingvector_x, homingvector_y;
 static float motion_x, motion_y;
 static uint8_t make_snapshot = 0;
+static uint8_t follow_vector = 0;
 uint8_t msg_id_check = 0;
 uint8_t use_stereoboard = 1;
 /*struct stereocam_t stereocam = {
-   .device = 0,
-   .msg_available = false
+ .device = 0,
+ .msg_available = false
  };*/
 struct UartDataStruct USART1_Data;
-uint8_t msg_buf[4*64];           // define local data
+uint8_t msg_buf[4 * 64];           // define local data
 
 /* struct pprz_transport pprz;
  struct link_device dev;*/
-struct uint8array {
+struct uint8array
+{
   uint8_t len;
   uint8_t height;
   uint8_t *data;
   bool data_new;
 };
 
+static setpoint_t sp;
 
 void stereoboardTask(void* arg)
 {
 
   systemWaitStart();
-  struct uint8array stereocam_data = {.len = 0, .data = msg_buf, .data_new = 0, .height = 0}; // buffer used to contain image without line endings
+  struct uint8array stereocam_data = { .len = 0, .data = msg_buf, .data_new = 0,
+      .height = 0 };  // buffer used to contain image without line endings
 
-  while(1){
-    pprz_check_and_parse(&dev, &pprz, stereocam_data.data, &stereocam_data.data_new);
-
+  while (1) {
+    pprz_check_and_parse(&dev, &pprz, stereocam_data.data,
+        &stereocam_data.data_new);
 
     uint8_t msg_id = stereocam_data.data[1];
     msg_id_check = msg_id;
 
-    motion_x = (float)msg_id;
+    motion_x = (float) msg_id;
     switch (msg_id) {
-
-
-      case 90: {
-
-       homingvector_x = DL_STEREOCAM_VISUALHOMING_X(stereocam_data.data);
-       homingvector_y = DL_STEREOCAM_VISUALHOMING_Y(stereocam_data.data);
-
+      case 90:
+        homingvector_x = DL_STEREOCAM_VISUALHOMING_X(stereocam_data.data);
+        homingvector_y = DL_STEREOCAM_VISUALHOMING_Y(stereocam_data.data);
         pprz_msg_send_STEREOCAM_VISUALHOMING_COMMAND(&(pprz.trans_tx), &dev, 0,
-          &make_snapshot );
-        break;
+            &make_snapshot);
 
-
-      }
-
-      case 81: {
-
-        //float res = (float)DL_STEREOCAM_VELOCITY_resolution(stereocam_data.data);
-
-
-        velx = (float)DL_STEREOCAM_VELOCITY_velx(stereocam_data.data);
-        vely = (float)DL_STEREOCAM_VELOCITY_vely(stereocam_data.data);
-        velz = (float)DL_STEREOCAM_VELOCITY_velz(stereocam_data.data);
-
-        float Npix = 30.0f;
-        float thetapix = 0.07f;
-        float rad_to_pixel = Npix / thetapix;
-
-
-       //motion_x = rad_to_pixel*(float)atan((velz * 0.01f)*0.001f/(range_last * 0.001f));
-       motion_y = rad_to_pixel*(float)atan((velx * 0.01f)*0.001f/(range_last * 0.001f));
-
-        /*flowMeasurement_t flowData;
-       flowData.stdDevX = 0.25;    // [pixels] should perhaps be made larger?
-       flowData.stdDevY = 0.25;    // [pixels] should perhaps be made larger?
-       flowData.dt = 0.01;
-       flowData.dpixelx = motion_x;
-       flowData.dpixely = motion_y;
-
-      if (abs(motion_x) < 100 && abs(motion_y) < 100 && range_last > 100 && use_stereoboard == 1)
-           estimatorKalmanEnqueueFlow(&flowData);*/
-
-       /*float noise = 1-(float)DL_STEREOCAM_VELOCITY_vRMS(stereocam_msg_buf)/res;
-
-       // Rotate camera frame to body frame
-       struct FloatVect3 body_vel;
-       float_rmat_transp_vmult(&body_vel, &stereocam.body_to_cam, &camera_vel);
-         */
+        if(follow_vector) {
+          memset(&sp, 0, sizeof(sp));
+          sp.mode.x = modeVelocity;
+          sp.mode.y = modeVelocity;
+          sp.mode.z = modeVelocity;
+          sp.velocity.x = homingvector_x;
+          sp.velocity.y = homingvector_y;
+          sp.velocity.z = 0.0;
+          sp.mode.yaw = modeVelocity;
+          sp.attitudeRate.yaw = 0.0;
+          commanderSetSetpoint(&sp, COMMANDER_PRIORITY_HOMING);
+        }
 
         break;
-      }
 
+      default:
+        break;
     }
     //DEBUG_PRINT("%d, check!\n",vel);
 
     vTaskDelay(10);
   }
 }
-
 
 /* Initialize the deck driver */
 void stereoboardDeckInit(DeckInfo *info)
@@ -143,12 +120,10 @@ void stereoboardDeckInit(DeckInfo *info)
 
   DEBUG_PRINT("Test StereoboardDeck!\n");
 
-  xTaskCreate(stereoboardTask, UART_RX_TASK_NAME, UART_RX_TASK_STACKSIZE, NULL, 3, NULL);
-
+  xTaskCreate(stereoboardTask, UART_RX_TASK_NAME, UART_RX_TASK_STACKSIZE, NULL,
+      3, NULL);
 
 }
-
-
 
 static const DeckDriver stereoboard_deck = {
     .vid = 0xBC,
@@ -179,7 +154,7 @@ LOG_ADD(LOG_UINT8, msg_id_check, &msg_id_check)
 
 LOG_GROUP_STOP(monocam)
 
-
 PARAM_GROUP_START(visualhoming)
 PARAM_ADD(PARAM_UINT8, make_snapshot, &make_snapshot)
+PARAM_ADD(PARAM_UINT8, follow_vector, &follow_vector)
 PARAM_GROUP_STOP(visualhoming)
